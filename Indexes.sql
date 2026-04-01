949bf2f621930baaf882ea2c21c5018e8ff52142
ozonen <oz.onen@hotmail.com>
2026-04-01 12:12:38 +0200

    Organize files into sql and diagrams folders

--- Indexes.sql
+++
@@ -1,7 +0,0 @@
-USE ecommerce_db;
-
-CREATE INDEX idx_products_category  ON products(category_id);
-CREATE INDEX idx_orders_customer    ON orders(customer_id);
-CREATE INDEX idx_order_items_order  ON order_items(order_id);
-CREATE INDEX idx_payments_order     ON payments(order_id);
-CREATE INDEX idx_reviews_product    ON reviews(product_id);
