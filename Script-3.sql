0c6ac44027ba5a135f116f79e356ead472b9aa6e
Özgür <oz.onen@hotmail.com>
2026-04-01 11:41:19 +0200

    Update Script-3.sql

--- Script-3.sql
+++ Script-3.sql
@@ -1,7 +1,7 @@
 USE ecommerce_db;
 
-SHOW INDEX FROM products;
-SHOW INDEX FROM orders;
-SHOW INDEX FROM order_items;
-SHOW INDEX FROM payments;
-SHOW INDEX FROM reviews;
+CREATE INDEX idx_products_category  ON products(category_id);
+CREATE INDEX idx_orders_customer    ON orders(customer_id);
+CREATE INDEX idx_order_items_order  ON order_items(order_id);
+CREATE INDEX idx_payments_order     ON payments(order_id);
+CREATE INDEX idx_reviews_product    ON reviews(product_id);
